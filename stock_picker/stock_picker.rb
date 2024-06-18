4fc7c43bcbbf15ca5499a7cc8d671a3d33f070c3
zekeer21 <achas241@gmail.com>
2024-06-18 15:34:06 +0800

    Initial commit

---
+++ stock_picker/stock_picker.rb
@@ -0,0 +1,24 @@
+prices = [17,3,6,9,15,8,6,1,10]
+
+def stock_picker(prices)
+  min_price_day = 0
+  max_profit = 0
+  best_days = [0, 0]
+
+  prices.each_with_index do |price, day|
+    if price < prices[min_price_day]
+      min_price_day = day
+    end
+
+    potential_profit = price - prices[min_price_day]
+
+    if potential_profit > max_profit
+      max_profit = potential_profit
+      best_days = [min_price_day, day]
+    end
+  end
+
+  best_days
+end
+
+p stock_picker(prices)
